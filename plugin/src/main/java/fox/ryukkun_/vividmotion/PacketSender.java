78ec2b4bc1e764bdb43dcb81de9e521f6e41bcb6
Ryukkun <tkytwins2@gmail.com>
2024-11-10 00:59:55 +0900

    ♲ 1.21.2でnmsのclass名が一部変わったみたい

--- plugin/src/main/java/fox/ryukkun_/vividmotion/PacketSender.java
+++ plugin/src/main/java/fox/ryukkun_/vividmotion/PacketSender.java
@@ -14,18 +14,18 @@ public class PacketSender {
     private final static PacketSendInterface connection;
 
     static {
-            if (MCVersion.greaterThanEqual(MCVersion.v1_20_5)) {
-                connection = new v1_20_5();
+        if (MCVersion.greaterThanEqual(MCVersion.v1_20_5)) {
+            connection = new v1_20_5();
 
-            } else if (MCVersion.greaterThanEqual(MCVersion.v1_17_R1)) {
-                connection = new v1_17_R1();
+        } else if (MCVersion.greaterThanEqual(MCVersion.v1_17_R1)) {
+            connection = new v1_17_R1();
 
-            } else if (MCVersion.greaterThanEqual(MCVersion.v1_14_R1)) {
-                connection = new v1_14_R1();
+        } else if (MCVersion.greaterThanEqual(MCVersion.v1_14_R1)) {
+            connection = new v1_14_R1();
 
-            } else {
-                connection = new lessThan_v1_14_R1();
-            }
+        } else {
+            connection = new lessThan_v1_14_R1();
+        }
     }
 
     public static void sendPacket(Player player, List<MapPacket> packets) {
@@ -51,7 +51,7 @@ public class PacketSender {
                 final Class<?> mapPacketClass = Class.forName("net.minecraft.network.protocol.game.PacketPlayOutMap");
                 final Class<?> connectionClass = Class.forName("net.minecraft.server.network.PlayerConnection");
                 final Class<?> playerClass = Class.forName("net.minecraft.server.level.EntityPlayer");
-                final Class<?> bClass = Class.forName("net.minecraft.world.level.saveddata.maps.WorldMap$b");
+                final Class<?> bClass = Class.forName("net.minecraft.world.level.saveddata.maps.WorldMap$"+(MCVersion.greaterThanEqual(MCVersion.v1_21_2) ? "c" : "b"));
                 final Class<?> packetClass = Class.forName("net.minecraft.network.protocol.Packet");
                 final Class<?> mapIdClass = Class.forName("net.minecraft.world.level.saveddata.maps.MapId");
 
